8f33e358d069e392e24c411554b1ff6811cc9c0a
Matt Munro <matt@digitact.co.uk>
2022-08-03 22:52:34 +1000

    hide case picker

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
     entry: {
-        'case-picker': "./scripts/case-picker.js"
+        //'case-picker': "./scripts/case-picker.js"
     },
     module: {
 
