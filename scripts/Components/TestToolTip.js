e6c727cfccd7fbb2b9af81a25c9bb1d2abc86033
Matt Munro <matt@digitact.co.uk>
2022-08-04 17:03:27 +1000

    get rid of case picker from template

--- scripts/Components/TestToolTip.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const TestToolTip = () => {
-    return (
-<p>REACT CART</p>
-    )
-
-}
--- scripts/case-picker.js
+++
@@ -1,9 +0,0 @@
-import {createRoot} from "react-dom/client"
-import React from "react"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import SideBySideDisplay from './Components/SideBySidePicker/SideBySideDisplay'
-
-const rootElement = document.getElementById("react-case-picker")
-const root = createRoot(rootElement)
-root.render(<SideBySideDisplay />)
